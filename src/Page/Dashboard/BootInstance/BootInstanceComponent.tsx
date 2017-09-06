1d2262cfa1b21b61fa84cb9b884cff72c3f86536
iwatendo <iwatendo@gmail.com>
2017-09-06 22:46:42 +0900

    トップページとインスタンスブートベージのレイアウト修正

--- src/Page/Dashboard/BootInstance/BootInstanceComponent.tsx
+++ src/Page/Dashboard/BootInstance/BootInstanceComponent.tsx
@@ -66,34 +66,26 @@ export default class BootInstanceComponent extends React.Component<BootInstanceP
                     <div className="sbj-dashbord-boot-instance-note">
                         <div className="mdl-card__supporting-text">
                             <h6 className="sbj-dashbord-boot-instance-note">
-                                上記ボタンを押すと、このブラウザを擬似的なサーバーにしたWebRTCサービスを起動します。<br />
-                                この画面では外部との通信を行いません。<br />
-                                プロフィールや設定の変更が可能ですが、外部にデータ送信はされずブラウザに保持しているデータのみを更新をします。<br />
+                                ホームインスタンスとは、ブラウザ上で動作する小さなサーバーと考えてください。<br />
                                 <br />
-                                ホームインスタンスの起動に成功した場合、招待URLが表示されるので、招待したいメンバーにURLを伝えてください。<br />
-                                （ただし大人数での利用はできません。PCのスペックや回線にも依存しますが数人での利用が適正です）<br />
+                                ホームインスタンスを起動すると、接続する為のURLが表示されます。<br />
+                                招待したいメンバーにURLを伝えてください。招待されたメンバーは、そのURLで起動したホームインスタンスに接続できます。<br />
+                                （ただし大人数での利用はできません。PCスペックや回線にも依存しますが、数名での利用が適正です）<br />
                                 <br />
-                                ブラウザがサーバーの役割を果たす為、ホームインスタンスの停止または、ブラウザを閉じると接続しているメンバーのクライアントも停止します。<br />
-                                また、起動の度に接続URLが変わります。終了したホームインスタンスのURLは再利用できないことに注意してください。<br />
+                                ブラウザがサーバーの役割をするため、ホームインスタンスの停止または、ブラウザを閉じると接続メンバーのクライアントも停止します。<br />
+                                また、ホームインスタンスは起動する度にURLが変わります。停止したホームインスタンスのURLは再利用できないことに注意してください。<br />
                                 <br />
-                                ホームインスタンスが停止しても、登録したプロフィールやチャットの内容は消えません。<br />
+                                ホームインスタンスは停止しても、登録したプロフィールやチャットの内容は消えません。<br />
                                 プロフィール情報は各ユーザーのブラウザに保存され、チャットの内容はホームインスタンスを起動したブラウザに保存されます。<br />
-                                <br />
-                                （例）Ａさんがホームインスタンスを起動して、Ｂさんとチャットした場合、Ａさんのブラウザにチャットの内容が保存されます。<br />
-                                後日、再度Ａさんがホームインスタンスを起動した場合、前回の内容が引き継がれますが、Ｂさんがホームインスタンスを起動してＡさんとチャットする場合、チャットの内容は引継がれません。<br />
-                                <br />
-                                （注意事項）チャット内容は、接続ユーザーに関わらず引継がれます。<br />
-                                例えば上記の状態で、後日、Ａさんがホームインスタンスを起動してＣさんを招待した場合、ＡさんとＢさんのチャットの内容がＣさんに見られる可能性がある事に注意してください。<br />
-                                <br />
-                                ※チャットの内容は、ホームインスタンスページの「タイムラインクリア」で削除可能です。<br />
+                                また、ホームインスタンスの起動前でも、プロフィールの変更やアクター登録は可能です。<br />
                             </h6>
                         </div>
                         <div className="mdl-card__supporting-text">
                             <h6 className="sbj-dashbord-boot-instance-note">
                                 補足ページ<br />
-                                ・<a id="architecrure" href="https://github.com/iwatendo/skybeje/wiki/Architecture" target="_blunk">ホームインスタンスとは？</a><br />
-                                ・<a id="security" href="https://github.com/iwatendo/skybeje/wiki/Security" target="_blunk">セキュリティ対策について</a><br />
-                                ・<a id="troubleshooting" href="https://github.com/iwatendo/skybeje/wiki/Troubleshooting" target="_blunk">トラブルシュート</a><br />
+                                　・<a id="architecrure" href="https://github.com/iwatendo/skybeje/wiki/Architecture" target="_blunk">ホームインスタンスとは？</a><br />
+                                　・<a id="security" href="https://github.com/iwatendo/skybeje/wiki/Security" target="_blunk">セキュリティ対策について</a><br />
+                                　・<a id="troubleshooting" href="https://github.com/iwatendo/skybeje/wiki/Troubleshooting" target="_blunk">トラブルシュート</a><br />
                             </h6>
                         </div>
                     </div>
